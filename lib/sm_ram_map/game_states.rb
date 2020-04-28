4ee396b2f91d8cf8c2a705b480552906ca391f3e
Paul Brannan <cout@users.noreply.github.com>
2020-04-28 05:06:02 -0400

    Invert game states
    Inverted the lookup table for game states; mapping was from state name
    to value but should have been the other way around.

--- lib/sm_ram_map/game_states.rb
+++ lib/sm_ram_map/game_states.rb
@@ -1,13 +1,15 @@
 GAME_STATES = {
-  loading:                 0x06,
-  loading2:                0x07, # ?
-  normalGameplay:          0x08,
-  doorTransition:          0x0B,
-  pausing:                 0x0E,
-  inPauseMenu:             0x0F,
-  leavingPauseMenu:        0x10,
-  leavingPauseMenu2:       0x12, # ?
-  startOfCeresCutscene:    0x20,
-  preEndCutscene:          0x26, # briefly at this value during the black screen transition after the ship fades out
-  endCutscene:             0x27,
+  0x06 => :loading,
+  0x07 => :loading2,
+  0x08 => :normalGameplay,
+  0x0B => :doorTransition,
+  0x0E => :pausing,
+  0x0F => :inPauseMenu,
+  0x10 => :leavingPauseMenu,
+  0x12 => :leavingPauseMenu2,
+  0x20 => :startOfCeresCutscene,
+  0x21 => :ceresCutscene1,
+  0x22 => :ceresCutscene2,
+  0x26 => :preEndCutscene, # briefly at this value during the black screen transition after the ship fades out
+  0x27 => :endCutscene,
 }
